3ac97a75aca40c336f5fd5c0089e1a00c38a222e
DameDanny <161458137+DameDanny@users.noreply.github.com>
2024-10-22 14:34:52 +0200

    Exercice state.1

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,26 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
 	{
 		name: "bulbasaur",
 		imgSrc:
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
 	},
 	{
 		name: "mew",
@@ -14,10 +30,30 @@ const pokemonList = [
 import "./App.css";
 
 function App() {
+	const [pokemonIndex, setNumber] = useState(0);
+	const booleanPokemon = pokemonIndex > 0;
+	const booleanPokemon2 = pokemonIndex < pokemonList.length - 1;
+	const previousClick = () => {
+		setNumber(pokemonIndex - 1);
+	};
+	const nextClick = () => {
+		setNumber(pokemonIndex + 1);
+	};
 	return (
 		<div>
-			<p>Hello</p>
-			<PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+			<p>Hello pokemon number {pokemonIndex}</p>
+			<PokemonCard
+				name={pokemonList[pokemonIndex].name}
+				imgSrc={pokemonList[pokemonIndex].imgSrc}
+			/>
+			<div>
+				{booleanPokemon ? (
+					<button onClick={previousClick}>Previous</button>
+				) : (
+					<br />
+				)}
+				{booleanPokemon2 ? <button onClick={nextClick}>Next</button> : <br />}
+			</div>
 		</div>
 	);
 }
